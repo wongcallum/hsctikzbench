05288d30d2edfbfee8c4e735d39a6bb8cf3067ca
Callum Wong <mail@callumwong.com>
2026-09-09 21:35:10 +1000

    body margin 0

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,4 +1,5 @@
 import "@radix-ui/themes/styles.css";
+import "./index.css";
 import { Theme } from "@radix-ui/themes";
 import { ThemeProvider } from "next-themes";
 import { StrictMode } from "react";
